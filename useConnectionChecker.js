61d3af5a0f0eb78753b1038a3ae66fe8fb321932
faridEsnaashari <faride13578642@gmail.com>
2021-03-20 21:44:45 +0330

    init

---
+++ useConnectionChecker.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useSocketReConnector = (checkTimeInterval, checkTimeOut, url = window.location.origin.toString()) => {
+    const [ connection, setConnection ] = useState(true);
+
+    async function* checkConnection(){
+        while(true){
+            try{
+                const connectionsCheckResult = await axios.get(url);
+                yield;
+            }
+            catch(err){
+                if(err.message.toString() === "Network Error"){
+                    break;
+                }
+                yield;
+            }
+        }
+
+        yield "checkAgain";
+
+        try{
+            const connectionsCheckResult = await axios.get(url);
+        }
+        catch(err){
+            if(err.message.toString() === "Network Error"){
+                setConnection(false);
+                return;
+            }
+        }
+    }
+
+    useEffect(() => {
+        const connectionChecker = checkConnection();
+
+        const connectionsCheckTimer = setInterval(() => {
+            const status = connectionChecker.next().value;
+            if(status === "checkAgain"){
+                const connectionsCheckAndConnectTimer = setTimeout(connectionChecker.next(), checkTimeOut);
+            }
+        }, checkTimeInterval);
+
+        return () => clearInterval(connectionsCheckTimer);
+    }, []);
+
+    return connection;
+};
+
+export default useSocketReConnector;
